49a92432dc9c1bbe6f67d6bb6845c665e77b6283
Mikhail <mk1981000@gmail.com>
2016-08-16 23:51:09 +0300

    Update files

--- plugins/init-terminal.el
+++ plugins/init-terminal.el
@@ -3,11 +3,22 @@
 ;; Terminal emulator setting
 ;; ---------------------------
 
+(defun term-toggle-mode ()
+  "Swither between `line-mode' and `char-mode'."
+  (interactive)
+  (if (term-in-line-mode) 
+      (term-char-mode)
+      (term-line-mode)))
+
 (defun my-term-mode-config ()
   "Modify keymaps used by `term-mode'."
   (local-set-key (kbd "M-s") 'other-window)
+  ;; (local-set-key (kbd "<f9>") 'term-toggle-mode)
   ;; more here
   )
+
+(global-set-key (kbd "<f9>") 'term-toggle-mode)
+
 
 ;; add to hook
 (add-hook 'term-mode-hook 'my-term-mode-config)
