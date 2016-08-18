0cfdbdd80ab4af5246033e3a6fce758aed435bc6
Mikhail <mk1981000@gmail.com>
2016-08-18 23:54:19 +0300

    Update files

--- plugins/init-themes.el
+++ plugins/init-themes.el
@@ -1,10 +1,10 @@
 ;; init-themes.el
 ;; -----------------------
 
-;; (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
-(use-package sublime-themes
-  :ensure t)
+;; (use-package sublime-themes
+;;   :ensure t)
 
 (use-package gruvbox-theme
   :ensure t
