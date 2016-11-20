169106d5507fb23ce3686e82f17ee43108fc5044
Mikhail <mk1981000@gmail.com>
2016-11-21 01:48:36 +0300

    Update files

--- plugins/init-files.el
+++ plugins/init-files.el
@@ -7,4 +7,6 @@
 (add-to-list 'auto-mode-alist '("\\.sh\\'" . shell-script-mode))
 (add-to-list 'auto-mode-alist '("\\.plugin.zsh\\'" . shell-script-mode))
 
+(add-to-list 'magic-mode-alist '("# required by " . conf-mode))
+
 (provide 'init-files)
