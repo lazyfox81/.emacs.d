245a96c8085f1cd11f59fbc216bdea4aa6bf13df
Mikhail <mk1981000@gmail.com>
2017-04-11 00:39:13 +0300

    Update files

--- plugins/init-themes.el
+++ plugins/init-themes.el
@@ -6,17 +6,20 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
 
 (use-package sublime-themes
-  :ensure t)
+  :ensure t
+  :disabled t)
 
 (use-package gruvbox-theme
   :ensure t
   :disabled t)
 
-(load-theme 'spolsky t)
+;; (load-theme 'spolsky t)
+(load-theme 'xresources t)
 
-(custom-set-faces
- '(default ((t (:background "#222222"))))
- '(fringe ((t (:background  "#222222")))))
+;; (custom-set-faces
+;;  '(default         ((t (:background  "#282828"))))
+;;  '(fringe          ((t (:background  "#282828"))))
+;;  '(vertical-border ((t (:foreground  "#282828")))))
 
 (provide 'init-themes)
 ;;--------------------------------------------------------------------
