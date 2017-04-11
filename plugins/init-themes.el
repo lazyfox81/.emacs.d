c8027160c2dbdee30c22310d317142c43010db9a
Mikhail <mk1981000@gmail.com>
2017-04-11 21:47:11 +0300

    Update files

--- plugins/init-themes.el
+++ plugins/init-themes.el
@@ -13,13 +13,13 @@
   :ensure t
   :disabled t)
 
-;; (load-theme 'spolsky t)
-(load-theme 'xresources t)
+(load-theme 'spolsky t)
+;; (load-theme 'xresources t)
 
-;; (custom-set-faces
-;;  '(default         ((t (:background  "#282828"))))
-;;  '(fringe          ((t (:background  "#282828"))))
-;;  '(vertical-border ((t (:foreground  "#282828")))))
+(custom-set-faces
+ '(default         ((t (:background  "#282828"))))
+ '(fringe          ((t (:background  "#282828"))))
+ '(vertical-border ((t (:foreground  "#282828")))))
 
 (provide 'init-themes)
 ;;--------------------------------------------------------------------
